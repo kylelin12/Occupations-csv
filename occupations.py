ab1af059c10ab3734729188bf9e4ee59e5dd7856
Linku <klin12@stuy.edu>
2017-09-15 14:32:29 -0400

    Finished Assignment

--- occupations.py
+++ occupations.py
@@ -15,16 +15,19 @@ occ_dic.pop("Job Class")
 occ_dic_float = dict((key, float(value)) for key, value in occ_dic.iteritems())
 
 # Prints to double check if everything went according to plan
-print occ_dic_float
+#print occ_dic_float
 
 # Defines a new function that takes an argument
 def percent_chance(dictionary):
-    ran_num = random.random() * 100.0 # Generates a random numberq between [0, 1)
-    total_chance = occ_dic_float.get("Total") # Sets the total as the one given
     occ_dic_float.pop("Total") # Removes the now unnecessary footer of the csv
-    for key, value in dictionary.items(): # For loop that goes through each value for each key in the dictionary
-        total_chance += value # Keeps track of the total chance
-        if ran_num <= total_chance:
-            return key
+    while True:
+        for key, value in dictionary.items(): # For loop that goes through each value for each key in the dictionary
+            #print chance
+            ran_num = random.random() * 99.8 # Generates a random number between [0, 1) * total chance.
+            #print ran_num
+            if ran_num <= value:
+                #print key
+                return key
+
 
 print "\nYour randomly (weighted) occupation is: " + percent_chance(occ_dic_float)
