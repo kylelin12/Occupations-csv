3c18f84fd2171ff600ad575992498804a3a29c45
Linku <klin12@stuy.edu>
2017-09-15 11:44:07 -0400

    Worked a bit

--- occupations.py
+++ occupations.py
@@ -7,14 +7,24 @@ occ_list = csv.reader(open('occupations.csv', 'r'))
 # Function that converts a list to a dictionary
 occ_dic = dict(occ_list)
 
-# Removes the unnecessary header
+# Removes the unnecessary header of the csv
 occ_dic.pop("Job Class")
 
+
 # Converts the value to a float (stored as a number)
-dict((key, float(value)) for key, value in occ_dic.iteritems())
+occ_dic_float = dict((key, float(value)) for key, value in occ_dic.iteritems())
 
 # Prints to double check if everything went according to plan
-print occ_dic
+print occ_dic_float
 
-#def percent_chance(dictionary):
- #   total_chance = 
+# Defines a new function that takes an argument
+def percent_chance(dictionary):
+    ran_num = random.random() * 100.0 # Generates a random numberq between [0, 1)
+    total_chance = occ_dic_float.get("Total") # Sets the total as the one given
+    occ_dic_float.pop("Total") # Removes the now unnecessary footer of the csv
+    for key, value in dictionary.items(): # For loop that goes through each value for each key in the dictionary
+        total_chance += value # Keeps track of the total chance
+        if ran_num <= total_chance:
+            return key
+
+print "\nYour randomly (weighted) occupation is: " + percent_chance(occ_dic_float)
